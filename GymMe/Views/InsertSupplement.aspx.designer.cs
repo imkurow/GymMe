f5b7364a2a761e3814e966cd3f8ff88c032edad6
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-06-09 20:30:44 +0700

    Memodifikasi agar dapat terhubung dengan Manage Supplement page

--- GymMe/Views/InsertSupplement.aspx.designer.cs
+++ GymMe/Views/InsertSupplement.aspx.designer.cs
@@ -33,6 +33,15 @@ namespace GymMe.Views
         protected global::System.Web.UI.WebControls.LinkButton LinkButtonNav;
 
         /// <summary>
+        /// LinkButtonBack control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButtonBack;
+
+        /// <summary>
         /// LabelSupplement control.
         /// </summary>
         /// <remarks>
