76259b2965281dd71df0feb7f6324e3d936a10b8
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-06-09 19:37:29 +0700

    Menambahkan ProductController

---
+++ GymMe/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace GymMe.Controllers
+{
+    public class ProductController
+    {
+        public static bool isContainSupplement(string supplementName)
+        {
+            return supplementName.IndexOf("Supplement", StringComparison.OrdinalIgnoreCase) >= 0;
+        }
+
+        public static bool isGreaterThanToday(DateTime date)
+        {
+            DateTime today = DateTime.Today;
+            if (date > today)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool isValidPrice(int price)
+        {
+            if (price >= 3000)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
