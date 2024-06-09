d51b45711cdcec9951c915ea3a2e449415b8639a
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-06-09 14:08:30 +0700

    Membuat UserFactory

---
+++ GymMe/Factories/UserFactory.cs
@@ -0,0 +1,24 @@
+﻿using GymMe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GymMe.Factories
+{
+    public class UserFactory
+    {
+        public static MsUser createUser(string username, string email, string password, string gender, DateTime bod)
+        {
+            MsUser user = new MsUser();
+            user.UserName = username;
+            user.UserEmail = email;
+            user.UserPassword = password;
+            user.UserGender = gender;
+            user.UserRole = "Customer";
+            user.UserDOB = bod;
+            return user;
+        }
+
+    }
+}
