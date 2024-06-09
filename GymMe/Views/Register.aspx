f1151635a89b9a54681a501f4560814c537c4ac6
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-06-09 14:25:30 +0700

    Membuat fitur Register

---
+++ GymMe/Views/Register.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Register.aspx.cs" Inherits="GymMe.Views.Register" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="LabelUsername" runat="server" Text="Username"></asp:Label>
+            <asp:TextBox ID="TextBoxUsername" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="LabelUsernameError" runat="server" Text=""></asp:Label>
+        </div>
+
+        <div>
+             <asp:Label ID="LabelEmail" runat="server" Text="Email"></asp:Label>
+             <asp:TextBox ID="TextBoxEmail" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="LabelEmailError" runat="server" Text=""></asp:Label>
+        </div>
+
+        <div>
+            <asp:Label ID="LabelGender" runat="server" Text="Gender"></asp:Label>
+            <asp:DropDownList ID="DropDownListGender" runat="server">
+                <asp:ListItem Text="Male" Value="Male"></asp:ListItem>
+                <asp:ListItem Text="Female" Value="Female"></asp:ListItem>
+            </asp:DropDownList>
+        </div>
+        <div>
+            <asp:Label ID="LabelGenderError" runat="server" Text=""></asp:Label>
+        </div>
+
+        <div>
+             <asp:Label ID="LabelPassword" runat="server" Text="Password"></asp:Label>
+             <asp:TextBox ID="TextBoxPassword" TextMode="Password" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="LabelPasswordError" runat="server" Text=""></asp:Label>
+        </div>
+
+        <div>
+             <asp:Label ID="LabelPassword2" runat="server" Text="Password Confirmation"></asp:Label>
+             <asp:TextBox ID="TextBoxPassword2" TextMode="Password" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="LabelPassword2Error" runat="server" Text=""></asp:Label>
+        </div>
+
+        <div>
+             <asp:Label ID="LabelDOB" runat="server" Text="Date of Birth"></asp:Label>
+            <asp:Calendar ID="CalendarDOB" runat="server"></asp:Calendar>
+        </div>
+        <div>
+            <asp:Label ID="LabelDOBError" runat="server" Text=""></asp:Label>
+        </div>
+            <asp:LinkButton ID="LinkButtonLogin" runat="server" OnClick="LinkButtonLogin_Click">Login Button</asp:LinkButton>
+        <div>
+            <asp:Button ID="ButtonRegister" runat="server" Text="Register" OnClick="ButtonRegister_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ GymMe/Views/Register.aspx.cs
@@ -0,0 +1,100 @@
+﻿using GymMe.Controllers;
+using GymMe.Factories;
+using GymMe.Models;
+using GymMe.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GymMe.Views
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LinkButtonLogin_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("login.aspx");
+        }
+
+        protected void ButtonRegister_Click(object sender, EventArgs e)
+        {
+            int isvalid = 1;
+
+            string username = TextBoxUsername.Text;
+            string email = TextBoxEmail.Text;
+            string password = TextBoxPassword.Text;
+            string password2 = TextBoxPassword2.Text;
+            string gender = DropDownListGender.SelectedValue;
+            DateTime bod = CalendarDOB.SelectedDate;
+
+            if (username.Length < 5 || username.Length > 15)
+            {
+                LabelUsernameError.Text = "Minimum is 5 and max is 15";
+                isvalid = 0;
+            }
+            if (!UserController.isValidEmail(email))
+            {
+                LabelEmailError.Text = "Email must end with .com";
+                isvalid = 0;
+            }
+            if (!UserController.IsPasswordAlphanumeric(password))
+            {
+                LabelPasswordError.Text = "Password must alphanumeric";
+                isvalid = 0;
+            }
+            if (!(password.Equals(password2)))
+            {
+                LabelPassword2.Text = "Password and password confirmation not match";
+                isvalid = 0;
+            }
+            if (!UserController.IsPasswordAlphanumeric(password2))
+            {
+                LabelPassword2Error.Text = "Password must alphanumeric";
+                isvalid = 0;
+            }
+            if (username.Equals(""))
+            {
+                LabelUsernameError.Text = "Username cant empty";
+                isvalid = 0;
+            }
+            if (email.Equals(""))
+            {
+                LabelEmailError.Text = "Email cant empty";
+                isvalid = 0;
+            }
+            if (password.Equals(""))
+            {
+                LabelPasswordError.Text = "Password cant empty";
+                isvalid = 0;
+            }
+            if (password2.Equals(""))
+            {
+                LabelPassword2Error.Text = "Password confirmation cant empty";
+                isvalid = 0;
+            }
+            if (gender.Equals(""))
+            {
+                LabelGenderError.Text = "Gender cant empty";
+                isvalid = 0;
+            }
+            if (bod.Equals(""))
+            {
+                LabelDOBError.Text = "Date of birth cant empty";
+                isvalid = 0;
+            }
+            if (isvalid == 1)
+            {
+                MsUser newUser = UserFactory.createUser(username, email, password, gender, bod);
+                UserRepository.CreateUser(newUser);
+                Response.Redirect("login.aspx");
+            }
+        }
+    }
+}
