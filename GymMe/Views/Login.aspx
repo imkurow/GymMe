c373608777a94fd355af0adffbdd229da7fd9da0
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-06-09 14:25:17 +0700

    Membuat fitur login

---
+++ GymMe/Views/Login.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="GymMe.Views.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+        <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="LabelUsername" runat="server" Text="Username"></asp:Label>
+            <asp:TextBox ID="TextBoxUsername" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="LabelUsernameError" runat="server" Text=""></asp:Label>
+        </div>
+        <div>
+            <asp:Label ID="LabelPassword" runat="server" Text="Password"></asp:Label>
+            <asp:TextBox ID="TextBoxPassword" TextMode="Password" runat="server"></asp:TextBox>
+        </div>
+         <div>
+            <asp:Label ID="LabelPassowordError" runat="server" Text=""></asp:Label>
+        </div>
+        <div>
+            <asp:LinkButton ID="LinkButtonRegister" runat="server" OnClick="LinkButtonRegister_Click">Register</asp:LinkButton>
+        </div>
+        <asp:Button ID="ButtonLogin" runat="server" Text="Login" OnClick="ButtonLogin_Click"/>
+    </form>
+</body>
+</html>
---
+++ GymMe/Views/Login.aspx.cs
@@ -0,0 +1,70 @@
+﻿using GymMe.Controllers;
+using GymMe.Models;
+using GymMe.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GymMe.Views
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["user"] != null || Request.Cookies["user_cookie"] != null)
+            {
+                Response.Redirect("navigation.aspx");
+            }
+        }
+
+        protected void LinkButtonRegister_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("register.aspx");
+        }
+
+        protected void ButtonLogin_Click(object sender, EventArgs e)
+        {
+            string username = TextBoxUsername.Text;
+            string password = TextBoxPassword.Text;
+
+            int isvalid = 1;
+
+            if (username.Length < 5 || username.Length > 15)
+            {
+                LabelUsernameError.Text = "Minimum is 5 and max is 15";
+                isvalid = 0;
+            }
+            if (username.Equals(""))
+            {
+                LabelUsernameError.Text = "Username cannot be empty";
+                isvalid = 0;
+            }
+            if (!UserController.IsPasswordAlphanumeric(password))
+            {
+                LabelPassowordError.Text = "Password must alphanumeric";
+                isvalid = 0;
+            }
+            if (password.Equals(""))
+            {
+                LabelPassowordError.Text = "Password cannot be empty";
+                isvalid = 0;
+            }
+            if (isvalid == 1)
+            {
+                bool isExist = UserRepository.IsExist(username, password);
+                if (isExist)
+                {
+                    MsUser user = UserRepository.GetUserByUsername(username);
+                    HttpCookie cookie = new HttpCookie("user_cookie");
+                    cookie.Value = user.UserID.ToString();
+                    cookie.Expires = DateTime.Now.AddHours(1);
+                    Response.Cookies.Add(cookie);
+                    Response.Redirect("navigation.aspx");
+                }
+            }
+        }
+    }
+}
