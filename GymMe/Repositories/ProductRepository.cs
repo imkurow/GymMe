e02397b7f3f4be25eb92dfe7968ebf93d8c804e0
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-06-09 13:59:33 +0700

    Membuat ProductRepository

---
+++ GymMe/Repositories/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using GymMe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GymMe.Repositories
+{
+    public class ProductRepository
+    {
+        public static List<MsSuplementType> GetSupplementTypes()
+        {
+            DatabaseEntities1 db = DatabaseSingleton.GetInstance();
+            return db.MsSuplementTypes.ToList();
+        }
+
+    }
+}
