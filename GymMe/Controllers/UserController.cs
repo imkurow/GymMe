2355c1e1a826b9018b3e46a374ec23315d4d6023
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-06-09 14:24:59 +0700

    Membuat UserController

---
+++ GymMe/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace GymMe.Controllers
+{
+    public class UserController
+    {
+        public static bool isValidEmail(string email)
+        {
+            return email.EndsWith(".com", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static bool IsPasswordAlphanumeric(string password)
+        {
+            string pattern = @"^[a-zA-Z0-9]+$";
+            return Regex.IsMatch(password, pattern);
+        }
+    }
+}
