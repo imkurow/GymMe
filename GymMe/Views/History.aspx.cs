53322e67bd2c433dd6993091b0a536eb407d63e1
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-06-11 14:50:38 +0700

    Membuat logic sementara untuk history

---
+++ GymMe/Views/History.aspx.cs
@@ -0,0 +1,38 @@
+﻿using GymMe.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GymMe.Views
+{
+    public partial class History : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Request.Cookies["user_cookie"] == null)
+                {
+                    Response.Redirect("login.aspx");
+                }
+                else
+                {
+                    string id = Request.Cookies["user_cookie"].Value;
+                    string role = UserRepository.GetUserRole(id);
+                    if (role == "Admin")
+                    {
+                        AdminChoice.Visible = true;
+                    }
+                    else if (role == "Customer")
+                    {
+                        CustomerChoice.Visible = true;
+                    }
+
+                }
+            }
+        }
+    }
+}
