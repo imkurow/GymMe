a24e2e68e94688e0949fa7c0819a28a103c68ad9
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-06-12 23:24:11 +0700

    Menghapus link untuk UpdateSupplement

--- GymMe/Views/ManageSupplement.aspx.designer.cs
+++ GymMe/Views/ManageSupplement.aspx.designer.cs
@@ -49,14 +49,5 @@ namespace GymMe.Views
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.LinkButton LinkButtonInsertSup;
-
-        /// <summary>
-        /// LinkButtonUpdateSup control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton LinkButtonUpdateSup;
     }
 }
